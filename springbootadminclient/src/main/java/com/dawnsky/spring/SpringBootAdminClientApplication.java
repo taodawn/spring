75f7bb9408f4920d3f74ef63f016f9ed364a873c
taodawn <taodawn@gmail.com>
2018-12-27 08:05:13 +0800

    spring boot admin client

---
+++ springbootadminclient/src/main/java/com/dawnsky/spring/SpringBootAdminClientApplication.java
@@ -0,0 +1,9 @@
+package com.dawnsky.spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class,args);
+    }
+}
